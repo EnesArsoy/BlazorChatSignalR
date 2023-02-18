7f74804523d62e4a2036fb81bef08f58fa8b4a56
ENES Arısoy <enesarsoy05@gmail.com>
2023-02-19 02:43:00 +0300

    Add project files.

---
+++ BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Cryptography.Xml;
+
+namespace BlazorChatSignalR.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await SendMessage("", "User connected!");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("GetThatMessageDude", user, message);
+        }
+    }
+}
+
